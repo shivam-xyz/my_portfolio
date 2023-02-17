d92027b0bfae320b0d6ec377cf7297734cde1d63
shivam-xyz <75421877+shivam-xyz@users.noreply.github.com>
2023-02-17 17:19:10 +0530

    New Project Added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,31 +1,8 @@
-import { Avatar, Box, Button, Card, Container, Grid, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import { Box, Button, Card, Grid, Typography } from '@mui/material';
 import React from 'react';
-import my_image from '../assets/images/me1.jpg';
 // import GitHubIcon from '@mui/icons-material/GitHub';
 import AssignmentIcon from '@mui/icons-material/Assignment';
-import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import GpsFixedIcon from '@mui/icons-material/GpsFixed';
-import GTranslateRoundedIcon from '@mui/icons-material/GTranslateRounded';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import SubdirectoryArrowRightIcon from '@mui/icons-material/SubdirectoryArrowRight';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import css from '../assets/images/css.png';
-import django from '../assets/images/django.png';
-import flutter from '../assets/images/flutter.png';
-import html from '../assets/images/html.png';
-import javascript from '../assets/images/javascript2.png';
-import mongodb from '../assets/images/mongodb.png';
-import node from '../assets/images/node3.png';
-import python from '../assets/images/python.png';
-import react from '../assets/images/react1.png';
-import restapi from '../assets/images/restapi4.png';
-import techBanner from '../assets/images/techBanner.png';
-import meta from '../assets/images/meta.png'
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import Self_Intro from './Self_Intro';
-import What_i_do from './What_i_do';
 
 const Projects = () => {
     const arrProjects = [
